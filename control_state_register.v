85d9ee6b97dc2d3517afef5afe7e3d62d1c1c28c
bradyk-017 <sloth-ayes.0t@icloud.com>
2026-04-17 21:32:14 -0500

    Updated control unit code
    Updated control unit code with code I was able to test and compile in Verilog.

--- control_state_register.v
+++ control_state_register.v
@@ -1,31 +1,32 @@
-module control_state_machine(clk_i, reset_i, g_i, z_i, Q_o)
+module control_state_register(clk_i, reset_i, z_i, Q_o);
     input wire clk_i, reset_i, z_i;
-    output [1:0] reg Q_o;
+    output wire [1:0] Q_o;
 
     reg [1:0] state;
     reg [1:0] next_state;
 
-    Q_o = state;
+    assign Q_o = state;
 
     // Next-state combinational logic.
-    always @(Qstate, z_i) begin
+    always @(state, z_i) begin
         case(state)
             2'b00: next_state = 2'b01;
             2'b01: next_state = 2'b10;
-            2'b10: begin
-                next_state = z_i ? 2'b00 : 2'b01;
+				2'b10: next_state = 2'b11;
+            2'b11: begin
+                next_state = z_i ? 2'b10 : 2'b11;
             end
-            default: next_state = 2'b00
+				default: next_state = 2'b00;
         endcase
     end
 
     // State register block
-    always @(posedge clk_i, reset_i) begin
+    always @(posedge clk_i, posedge reset_i) begin
         if (reset_i) begin
-            state <= 2'b000
+            state <= 2'b00;
         end
         else begin
-            state <= next_state
+            state <= next_state;
         end
     end
 endmodule
